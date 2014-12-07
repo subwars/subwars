7a7eaf1ce4e9674bccca78e16f1ee9f362fee214
JohnnyT <ubergeek3141@gmail.com>
2014-12-07 00:58:02 -0700

    Uses native navigator.geolocation

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,17 @@
-document.querySelector('x-geolocation').addEventListener('positionchange', function (e) {
-  var lat = e.detail.latitude,
-      lng = e.detail.longitude,
-      geohash = encodeGeoHash(lat, lng);
+function positionUpdated(position){
+  var lat = position.coords.latitude,
+    lng = position.coords.longitude,
+    accuracy = position.coords.accuracy,
+    geohash = encodeGeoHash(lat, lng);
 
   console.log('latitude:', lat);
   console.log('longitude:', lng);
+  console.log('accuracy:', lng);
   console.log('geohash:', geohash);
 
   $('#lat').html(lat);
   $('#lng').html(lng);
+  $('#acc').html(accuracy);
   $('#geohash').html(geohash);
 
   $.get('/ping/'+geohash).done(function(data){
@@ -18,7 +21,15 @@ document.querySelector('x-geolocation').addEventListener('positionchange', funct
 
   $.getJSON('/geocells/'+geohash).done(function(data){
     var jsonString = JSON.stringify(data, null, 4);
-
     $('#locations').prepend("<li><pre>"+jsonString+"</pre></li>");
   });
+};
+
+$(function(){
+  navigator.geolocation.watchPosition(
+    positionUpdated,
+    function(error){
+      window.alert(error)
+    }
+  )
 });
