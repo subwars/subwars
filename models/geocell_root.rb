8440f9f9ca59a22403383a7022e4d39350951485
JohnnyT <ubergeek3141@gmail.com>
2014-12-08 22:35:26 -0700

    Adds GeocellRoot

---
+++ models/geocell_root.rb
@@ -0,0 +1,24 @@
+class GeocellRoot
+  ROOT_KEY = :GCR
+  include Storable
+
+  attr_reader :name, :cell
+
+  def initialize(name)
+    @name = name
+    @cell = Geocell.new
+  end
+
+  def [](hash_string)
+    symbols = hash_string.split('').map{|char| char.to_sym}
+    cell[symbols]
+  end
+
+  def to_hash
+    {
+      class: self.class.name,
+      name: name,
+      cell: cell.to_hash
+    }
+  end
+end
