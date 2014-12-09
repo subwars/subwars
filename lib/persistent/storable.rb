302c6936d60525abb2a74d2221d42523c0279c0f
JohnnyT <ubergeek3141@gmail.com>
2014-12-08 22:34:23 -0700

    Adds persistent lib directory

--- lib/persistent/storable.rb
+++ lib/persistent/storable.rb
@@ -1,6 +1,8 @@
 module Storable
   def self.included(base)
     base.extend ClassMethods
+    base.store
+    base.attr_reader :uuid
   end
 
   module ClassMethods
@@ -8,12 +10,29 @@ module Storable
       self::ROOT_KEY
     end
 
+    def size
+      store.size
+    end
+
     def store
       Maglev[root_key] ||= IdentitySet.new
     end
+
+    def [](uuid)
+      store.detect{|o| o.uuid == uuid}
+    end
+
+    def new_uuid
+      '%s-%s' % [ root_key.to_s.upcase, `uuidgen`.chomp.upcase ]
+    end
+  end
+
+  def new_uuid!
+    @uuid = self.class.new_uuid if uuid.nil?
   end
 
   def stage
+    new_uuid!
     self.class.store.add self
   end
 
