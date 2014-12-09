ff832838b0cf060daa9c39b51f3bc87d0bef486f
JohnnyT <ubergeek3141@gmail.com>
2014-12-08 22:34:47 -0700

    Updates commit task

--- lib/tasks/commit.rb
+++ lib/tasks/commit.rb
@@ -2,14 +2,25 @@ desc 'Commit the code'
 task :commit do
   $LOAD_PATH << File.expand_path('../..', __FILE__)
 
-  puts 'Loading models:'
+  load_proc = Proc.new do |path|
+    puts '  - %s' % File.basename(path, '.rb')
+    load path
+  end
 
   Maglev.persistent do
-    load './models/entity.rb'
+    puts 'Loading libs:'
+    Dir['./lib/persistent/*.rb'].each do |lib|
+      load_proc.call(lib)
+    end
+
+    puts 'Loading models:'
+    pre_models = %w[ entity geocell geocell_root ]
+    pre_models.each do |model|
+      load_proc.call("./models/#{model}.rb")
+    end
     Dir['./models/*.rb'].each do |model|
-      next if model =~ /entity\.rb$/
-      puts '  - %s' % File.basename(model)
-      load model
+      next if pre_models.any?{|pm| model =~ /#{pm}\.rb$/}
+      load_proc.call(model)
     end
   end
   Maglev.commit
