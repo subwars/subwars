364588cfc870d6b7e0ea65241a35dc560a872ce7
JohnnyT <ubergeek3141@gmail.com>
2014-12-08 22:35:49 -0700

    Adds Game

---
+++ models/game.rb
@@ -0,0 +1,34 @@
+class Game
+  ROOT_KEY = :GAM
+  include Storable
+
+  attr_accessor :name, :players, :entities, :geocell_root
+
+  def self.default
+    @@default ||= begin
+      new_game = self.new 'Default'
+      new_game.stage
+      new_game
+    end
+  end
+
+  def initialize(name)
+    @name = name
+    @players = IdentitySet.new
+    @entities = IdentitySet.new
+    @geocell_root = GeocellRoot.new name
+  end
+
+  def create_player(name)
+    Player.new(self, name)
+  end
+
+  def to_hash
+    {
+      class: self.class.name,
+      name: name
+    }
+  end
+end
+
+Game.default
