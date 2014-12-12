554f31e8a7246a3e3a4b4be8e64337b482df002c
JohnnyT <ubergeek3141@gmail.com>
2014-12-11 21:07:28 -0700

    Draws only needed cells

--- public/js/app.js
+++ public/js/app.js
@@ -4,32 +4,31 @@ function drawMap(){
   var chars = "0123456789bcdefghjkmnpqrstuvwxyz".split('');
   var parentCell;
 
-  $.each(chars, function(idx,gpChar){
-    $('#grid').append('<div id="gh-'+gpChar+'" class="gh-'+gpChar+'--"></div>');
-    $.each(chars, function(idx,parentChar){
-      $('#gh-'+gpChar).append('<div id="gh-'+gpChar+parentChar+'" class="gh-'+parentChar+'-"></div>');
-      $.each(chars, function(idx,kidChar){
-        $('#gh-'+gpChar+parentChar).append('<div id="gh-'+gpChar+parentChar+kidChar+'" class="gh black gh-'+kidChar+'"></div>');
-      });
-      //$('#map').append('<div class="ocean-gray geohash-'+c+'"></div>');
-    });
-  });
+  //$.each(chars, function(idx,gpChar){
+  //  $('#grid').append('<div id="gh-'+gpChar+'" class="gh-'+gpChar+'--"></div>');
+  //  $.each(chars, function(idx,parentChar){
+  //    $('#gh-'+gpChar).append('<div id="gh-'+gpChar+parentChar+'" class="gh-'+parentChar+'-"></div>');
+  //    $.each(chars, function(idx,kidChar){
+  //      $('#gh-'+gpChar+parentChar).append('<div id="gh-'+gpChar+parentChar+kidChar+'" class="gh black gh-'+kidChar+'"></div>');
+  //    });
+  //    //$('#map').append('<div class="ocean-gray geohash-'+c+'"></div>');
+  //  });
+  //});
 };
+
+function positionAcquired(position){
+  positionUpdated(position);
+  setTimeout(function(){
+    $('.gh-active')[0].scrollIntoView(true);
+  }, 200);
+}
 
 function positionUpdated(position){
   var lat = position.coords.latitude,
     lng = position.coords.longitude,
     accuracy = position.coords.accuracy,
     full_geohash = window.geohash.encode(lat, lng),
-    sub_geohash = '';
-
-  if (true || accuracy < 20) {
     sub_geohash = full_geohash.substr(0,8);
-    $('#map').addClass('vertical');
-  } else {
-    sub_geohash = full_geohash.substr(0,7);
-    $('#map').removeClass('vertical');
-  }
 
   if (currentGeohash == sub_geohash) {
     return;
@@ -40,16 +39,32 @@ function positionUpdated(position){
   $('#acc').html(accuracy);
   $('#geohash').html(sub_geohash);
 
-  $('#grid .ocean').removeClass('ocean').addClass('ocean-gray');
+  $('#grid .gh-active')
+    .removeClass('gh-active ocean')
+    .addClass('ocean-gray');
 
-  //$('#gh-'+sub_geohash.substr(sub_geohash.length-3))
-  var ghid = '#gh-'+sub_geohash.substr(sub_geohash.length-3);
-  $(ghid)
-    .removeClass('black')
-    .removeClass('ocean-gray')
-    .addClass('ocean');
+  var displayed_hash = sub_geohash.substr(sub_geohash.length-3);
 
-  $(ghid)[0].scrollIntoView(true);
+  var ancestors = '';
+  var parentCell;
+  $.each(displayed_hash.split(''), function(idx,currentChar){
+    parentCell = (ancestors.length == 0) ? $('#grid') : $('#gh-'+ancestors);;
+    ancestors += currentChar;
+
+    var domId = 'gh-'+ancestors;
+    var dashes = Array(3-idx).join('-'); // lol
+    var cssClass = 'gh-'+currentChar+dashes;
+
+    if ($(domId).length == 0) {
+      parentCell.append('<div id="'+domId+'" class="'+cssClass+'"></div>');
+    }
+  });
+
+  $('#gh-'+ancestors)
+    .removeClass('black ocean-gray')
+    .addClass('gh-active ocean');
+
+  //$(ghid)[0].scrollIntoView(true);
   console.log('accuracy:', accuracy);
   console.log('geohash:', sub_geohash);
 
@@ -59,7 +74,7 @@ function positionUpdated(position){
 $(function(){
   drawMap();
   navigator.geolocation.getAccurateCurrentPosition(
-    positionUpdated,
+    positionAcquired,
     function(err){console.log(err);},
     positionUpdated
   );
@@ -68,6 +83,6 @@ $(function(){
     function(error){
       console.log(error);
     },
-    { maximimAge: 0, enableHighAccuracy: true }
+    { enableHighAccuracy: true }
   )
 });
